4dd1bf9bc68d8dd79f757b256f72b13c8f341c61
Ali A <alsamawiali4@gmail.com>
2023-02-06 15:33:09 -0500

    playing with py ' '

--- test.py
+++ test.py
@@ -1,7 +1,40 @@
-# print(type(2))
-# hello world
+# age = int(input("enter your age "))
+# if age >= 13:
+#   print('you are good to go!')
+# else:
+#   print('you are not old enough!')
+# fav = input("What's your favorite programming language? ")
+# if fav == "Python":
+#   print('Nice choice!')
+# elif fav == "Golang":
+#   print("You're a cool kid I see...")
+# elif fav == 'JavaScript':
+#   print('Okay Mr. web developer.')
+# else:
+#   print("I don't know that language.")
 
-'''
-heloooo
-helooo
-'''
+
+# num1 = float(input("Enter a number: "))
+# if num1 >= 10 and num1 <=20:
+#   num2 = float(input("Enter another number: "))
+
+#   sum_of_nums = num1 + num2
+#   print("The sum of these two numbers is:",sum_of_nums)
+
+#   if sum_of_nums > 100:
+#     print("That is a large sum!")
+
+
+# list:
+# my_list=[]
+# my_str= input("Enter a string: ")
+# my_str = my_list.append
+# print(my_list)
+# print(my_str)
+
+word = input("Enter a word: ")
+word2 = input("Enter another word: ")
+if word in word2:
+  print("The first word is contained in the second one")
+else:
+  print("The first word isn't contained in the second one")
